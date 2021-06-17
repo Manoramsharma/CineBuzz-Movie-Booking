6b84ff204fd864de451727f8a24cab7a173fd512
Aditya Pandey <pandey.aditya4272@gmail.com>
2021-06-17 20:57:26 +0530

    Added Rate Us page

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,5 +1,4 @@
 const API_KEY ='api_key=abe4188594373161fabd84fc4a1aba16';
-
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
